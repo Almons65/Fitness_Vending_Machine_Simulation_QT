33cafccef2cb954e78d11d780ec44fd336418dd7
Almons <adisorn.almond@gmail.com>
2025-05-17 15:23:16 +0700

    Add files via upload

---
+++ build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_FitnessVen_FILE C:/Users/adiso/OneDrive/Documents/Coding/C++/GUI Vending Machine/FitnessVen/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/FitnessVen.exe)
+set(__QT_DEPLOY_TARGET_FitnessVen_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_FitnessVen_RUNTIME_DLLS C:/Qt/6.8.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
